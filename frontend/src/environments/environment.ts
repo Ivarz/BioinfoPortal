5c876f1eb4f33f3feeea38f34201a49e1bf0ea15
Ivarz <ivars.silamikelis@gmail.com>
2025-02-09 17:44:05 +0200

    env file

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiUrl: "https://bioinfo.biomed.lu.lv/api",
+  authority: "https://bioinfo.biomed.lu.lv/auth/realms/bioinfoportal",
+  secureRoutes: ['https://bioinfo.biomed.lu.lv/api/'],
+};
